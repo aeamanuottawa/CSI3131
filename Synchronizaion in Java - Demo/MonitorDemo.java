04ac04de9240beace7e1c8ccbe464b263ce0b972
Amir Eaman <aeaman@uottawa.ca>
2021-06-09 20:55:50 -0400

    update

--- Synchronizaion in Java - Demo/MonitorDemo.java
+++ Synchronizaion in Java - Demo/MonitorDemo.java
@@ -2,44 +2,58 @@ package demo;
 
 import java.util.concurrent.locks.ReentrantLock;
 
-class MonitorDemo extends Thread{
-    private  boolean mailDelivered = false;
-    private final static ReentrantLock lock = new ReentrantLock();
+class MonitorDemo extends Thread {
+  private boolean mailDelivered = false;
+  private final static ReentrantLock lock = new ReentrantLock();
 
-    public void checkMailbox(){
-        synchronized(lock){
-        	
-            while(! mailDelivered){
-                try { 
-                	  System.out.format("%s is waiting for their mail. \n",Thread.currentThread().getName());
-                lock.wait();
-              
-            } catch (InterruptedException e)  {
-                System.out.println("Something bad happened!"); }
-            }
-		  System.out.format("%s is reading their mail \n",Thread.currentThread().getName());
-		
-        }     
-    }
+  public void checkMailbox() {
+    synchronized(lock) {
 
-    public void postMan(){
-        synchronized(lock){
-    
-        	 System.out.format("Hello from %s, I have some mails!! \n",Thread.currentThread().getName());
-             this.mailDelivered = true;
-            
-             lock.notifyAll();
+      while (!mailDelivered) {
+        try {
+          System.out.format("%s is waiting for their mail. \n", Thread.currentThread().getName());
+          lock.wait();
+
+        } catch (InterruptedException e) {
+          System.out.println("Something bad happened!");
+        }
       }
-    }
-	
-	  public static void main(String args[]) {   
-		  MonitorDemo md = new MonitorDemo();
-		// new Thread( () -> { md.postMan(); }, "postMan1").start();
-    	new Thread( () -> { md.checkMailbox(); }, "Tenant1").start();
-    	new Thread( () -> { md.checkMailbox(); }, "Tenant2").start();
-    	try { Thread.sleep(3000); } catch(Exception e) {}
-		new Thread( () -> { md.postMan(); }, "postMan2").start();
+      System.out.format("%s is reading their mail \n", Thread.currentThread().getName());
 
-	  }
+    }
+  }
+
+  public void postMan() {
+    synchronized(lock) {
+
+      System.out.format("Hello from %s, I have some mails!! \n", Thread.currentThread().getName());
+      this.mailDelivered = true;
+
+      lock.notifyAll();
+    }
+  }
+
+  public static void main(String args[]) {
+    MonitorDemo md = new MonitorDemo();
+    // new Thread( () -> { md.postMan(); }, "postMan1").start();
+    new Thread(() -> {
+      md.checkMailbox();
+    }, "Tenant1").start();
 	
+	
+    new Thread(() -> {
+      md.checkMailbox();
+    }, "Tenant2").start();
+		
+    try {
+      Thread.sleep(3000);
+    } catch (Exception e) {}
+	
+	
+    new Thread(() -> {
+      md.postMan();
+    }, "postMan2").start();
+
+  }
+
 }
